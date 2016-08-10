439dd24445366dd775dc81b86bb33129f8bd0cc2
bjornmelgaard <bjornmelgaard@gmail.com>
2016-08-10 21:40:34 +0300

    reg work (allegedly)

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Todolist
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
